9379c715db2e28cfdc36b813984ce054776d1ce2
Oleh <vdovenko.oleg.1983@gmail.com>
2026-06-08 13:01:31 +0300

    create swipers and desktop for sections

--- src/js/playSteps.js
+++ src/js/playSteps.js
@@ -4,21 +4,22 @@ import 'swiper/css';
 function checkResolution() {
   const width = window.innerWidth;
 
-  const swiperTarget = document.querySelector('[data-box="swiper"]');
-  const wrapper = document.querySelector('[data-wrap="swiper-wrap"]');
+  const swiperTargets = document.querySelectorAll('[data-box="swiper"]');
+  const wrappers = document.querySelectorAll('[data-wrap="swiper-wrap"]');
   const slides = document.querySelectorAll('[data-slide="swiper-slide"]');
 
   if (width >= 1440) {
-    swiperTarget?.classList.remove('swiper');
-    wrapper?.classList.remove('swiper-wrapper');
+    swiperTargets.forEach(swiperTarget =>
+      swiperTarget.classList.remove('swiper')
+    );
+    wrappers.forEach(wrapper => wrapper.classList.remove('swiper-wrapper'));
     slides.forEach(slide => slide.classList.remove('swiper-slide'));
-    swiper.destroy(true, true);
   } else {
-    swiperTarget?.classList.add('swiper');
-    wrapper?.classList.add('swiper-wrapper');
+    swiperTargets.forEach(swiperTarget => swiperTarget.classList.add('swiper'));
+    wrappers.forEach(wrapper => wrapper.classList.add('swiper-wrapper'));
     slides.forEach(slide => slide.classList.add('swiper-slide'));
 
-    const swiper = new Swiper('#play-swiper', {
+    const swiper = new Swiper('.swiper', {
       direction: 'horizontal',
       loop: false,
       spaceBetween: 16,
@@ -26,5 +27,5 @@ function checkResolution() {
   }
 }
 
-window.addEventListener('load', checkResolution);
+window.addEventListener('DOMContentLoaded', checkResolution);
 window.addEventListener('resize', checkResolution);
