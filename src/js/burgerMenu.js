477ac9947c62765a81cdf5b952d17f8f1c1bae16
Oleh <vdovenko.oleg.1983@gmail.com>
2026-06-09 22:36:11 +0300

    fix bugs sections

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,19 +1,20 @@
-const openBtnEl = document.querySelector('[data-action="open"]');
-const closeBtnEl = document.querySelector('[data-action="close"]');
-const burgerMenuEl = document.querySelector('[data-visible]');
-const overlayEl = document.querySelector('[data-overvisible]');
+document.addEventListener('DOMContentLoaded', () => {
+  const openBtnEl = document.querySelector('[data-action="open"]');
+  const closeBtnEl = document.querySelector('[data-action="close"]');
+  const burgerMenuEl = document.querySelector('[data-visible]');
+  const navLinks = document.querySelectorAll('[data-nav-item]');
 
-openBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'open';
-  overlayEl.dataset.visible = 'open';
-});
+  openBtnEl.addEventListener('click', e => {
+    burgerMenuEl.dataset.visible = 'open';
+  });
 
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
-  overlayEl.dataset.visible = 'close';
-});
+  closeBtnEl.addEventListener('click', e => {
+    burgerMenuEl.dataset.visible = 'close';
+  });
 
-overlayEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
-  overlayEl.dataset.visible = 'close';
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      burgerMenuEl.dataset.visible = 'close';
+    });
+  });
 });
